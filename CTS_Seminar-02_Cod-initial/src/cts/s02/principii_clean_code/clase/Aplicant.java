fb1f3d84e06bc36311c9bd683c66576b1027d49c
SpringPurpur <vaceffv@gmail.com>
2026-02-27 08:54:54 +0200

    Seminar 2 CTS

---
+++ CTS_Seminar-02_Cod-initial/src/cts/s02/principii_clean_code/clase/Aplicant.java
@@ -0,0 +1,61 @@
+package cts.s02.principii_clean_code.clase;
+
+public abstract class Aplicant{
+	protected String nume;
+	protected String prenume;
+	protected int varsta;
+	protected int punctaj;
+	protected int nr_proiecte;
+	protected String[] denumireProiect;
+	private static int pragAcceptare = 80;
+
+	public Aplicant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Aplicant(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiect) {
+		super();
+		this.nume = nume;
+		this.prenume = prenume;
+		this.varsta = varsta;
+		this.punctaj = punctaj;
+		this.nr_proiecte = nr_proiecte;
+		this.denumireProiect = denumireProiect;
+	}
+
+	public String getNume() {
+		return nume;
+	}
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public String getPrenume() {
+		return prenume;
+	}
+	public void setPrenume(String prenume) {
+		this.prenume = prenume;
+	}
+	public int getVarsta() {
+		return varsta;
+	}
+	public void setVarsta(int varsta) {
+		this.varsta = varsta;
+	}
+	public void statut(){
+		System.out.println("Aplicantul "+nume+" "+prenume + ((this.punctaj > pragAcceptare) ? " a fost acceptat." : " nu a fost acceptat."));
+	}
+	public int getPunctaj() {
+		return punctaj;
+	}
+	public void setPunctaj(int punctaj) {
+		this.punctaj = punctaj;
+	}
+
+	public int getNumarProiecte() {
+		return nr_proiecte;
+	}
+	public void setNumarPproiecte(int nr_proiecte) {
+		this.nr_proiecte = nr_proiecte;
+	}
+}
